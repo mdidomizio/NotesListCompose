daddb430b3ba8a8a97e0caa400e800ddf6752404
mdidomizio <miriam.didomizio@googlemail.com>
2023-05-07 17:28:52 +0200

    installed room and created a database by using room

--- app/src/main/java/com/example/composesevennoteapp/model/Note.kt
+++ app/src/main/java/com/example/composesevennoteapp/model/Note.kt
@@ -1,12 +1,26 @@
 package com.example.composesevennoteapp.model
 
-import android.icu.text.CaseMap.Title
-import java.time.LocalDateTime
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
+import java.util.Date
 import java.util.UUID
 
-data class Note(
+
+@Entity(tableName = "notes_tbl")
+data class Note @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(), //serve a creare un uuid random
+
+    @ColumnInfo(name = "note_title")
     val title: String,
+
+    @ColumnInfo(name = "note_description")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+
+    @ColumnInfo(name = "note_entry_date")
+    val entryDate: Date = Date.from((Instant.now()))
 )
